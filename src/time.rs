4a967f440e15496ffb3de8cd6ca3c27243c78fbd
Victor <victor@caern.de>
2024-01-29 21:44:56 +0100

    Added documentation and examples.

--- src/time.rs
+++ src/time.rs
@@ -128,24 +128,77 @@ impl fmt::Debug for NormTime {
 	}
 }
 
+/// Normtime is formatted similar to ISO 8601 but with a "N" instead of a "T" between date and time. Also there is a year 0 as well as there are month and date with the number 0.
+///
+/// # Example
+///
+/// ```
+/// use normtime::NormTime;
+///
+/// let d = NormTime::from_ymd_opt( 0, 1, 1).unwrap();
+/// assert_eq!( d.to_string(), "0000-01-01N00:00:00" );
+///
+/// assert_eq!( "+12345-6-7".parse::<NormTime>().unwrap().to_string(), "12345-06-07N00:00:00" );
+///
+/// assert_eq!( "+12345-6-7N8:9:10".parse::<NormTime>().unwrap().to_string(), "12345-06-07N08:09:10" );
+/// ```
 impl fmt::Display for NormTime {
 	fn fmt( &self, f: &mut fmt::Formatter<'_> ) -> fmt::Result {
 		fmt::Debug::fmt( self, f )
 	}
 }
 
+/// Converting `chrono::NaiveDateTime` to `Normtime`.
+///
+/// # Example
+///
+/// ```
+/// use normtime::NormTime;
+/// use chrono::NaiveDate;
+///
+/// assert_eq!(
+/// 	NormTime::from( NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap().and_hms_opt( 0, 0, 1 ).unwrap() ),
+/// 	NormTime::from_ymd_opt( 0, 0, 0 ).unwrap().and_hms( 0, 0, 1 )
+/// );
+/// ```
 impl From<NaiveDateTime> for NormTime {
 	fn from( item: NaiveDateTime ) -> Self {
 		Self( item.timestamp() - NORMTIME_OFFSET )
 	}
 }
 
+/// Converting `chrono::NaiveDate` to `Normtime`. The time component is considered to be zero.
+///
+/// # Example
+///
+/// ```
+/// use normtime::NormTime;
+/// use chrono::NaiveDate;
+///
+/// assert_eq!(
+/// 	NormTime::from( NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap() ),
+/// 	NormTime::from_ymd_opt( 0, 0, 0 ).unwrap().and_hms( 0, 0, 0 )
+/// );
+/// ```
 impl From<NaiveDate> for NormTime {
 	fn from( item: NaiveDate ) -> Self {
 		Self::from( item.and_time( NaiveTime::from_num_seconds_from_midnight_opt( 0, 0 ).unwrap() ) )
 	}
 }
 
+/// Converting `Normtime` to `chrono::NaiveDateTime`.
+///
+/// # Example
+///
+/// ```
+/// use normtime::NormTime;
+/// use chrono::{NaiveDate, NaiveDateTime};
+///
+/// assert_eq!(
+/// 	NaiveDateTime::from( NormTime::from_ymd_opt( 0, 0, 0 ).unwrap().and_hms( 0, 0, 1 ) ),
+/// 	NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap().and_hms_opt( 0, 0, 1 ).unwrap()
+/// );
+/// ```
 impl From<NormTime> for NaiveDateTime {
 	fn from( item: NormTime ) -> Self {
 		let tstamp = NORMTIME_OFFSET + item.0;
@@ -153,6 +206,24 @@ impl From<NormTime> for NaiveDateTime {
 	}
 }
 
+/// Converting `Normtime` to `chrono::NaiveDate`. The `chrono::NaiveDate` does loose all time information that has a resolution finer than one standard day.
+///
+/// # Example
+///
+/// ```
+/// use normtime::NormTime;
+/// use chrono::NaiveDate;
+///
+/// assert_eq!(
+/// 	NaiveDate::from( NormTime::from_ymd_opt( 0, 0, 0 ).unwrap() ),
+/// 	NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap()
+/// );
+///
+/// assert_eq!(
+/// 	NaiveDate::from( NormTime::from_ymd_opt( 0, 0, 0 ).unwrap().and_hms( 0, 0, 1 ) ),
+/// 	NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap()
+/// );
+/// ```
 impl From<NormTime> for NaiveDate {
 	fn from( item: NormTime ) -> Self {
 		NaiveDateTime::from( item ).date()
@@ -233,20 +304,12 @@ mod tests {
 	fn create_normtime() {
 		// Unix-time zero.
 		let time_unix_zero = NaiveDateTime::from_timestamp_opt( 0, 0 ).unwrap();
-		let time_puls_zero = NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap().and_hms_opt( 0, 0, 0 ).unwrap();
+		let time_norm_zero = NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap().and_hms_opt( 0, 0, 0 ).unwrap();
 
 		assert_eq!( NormTime::from( time_unix_zero ), time_unix_zero );
-		assert_eq!( NormTime::from_timestamp_opt( time_puls_zero.timestamp() ).unwrap(), time_puls_zero );
-		assert_eq!( NormTime::from_ymd_opt( 0, 0, 0 ).unwrap(), time_puls_zero );
-		assert_eq!( NormTime::from_ymd_opt( 1, 0, 0 ).unwrap(), time_puls_zero + TimeDelta::seconds( 30_000_000 ) );
-	}
-
-	#[test]
-	fn normtime_to_naive_date() {
-		assert_eq!(
-			NaiveDate::from( NormTime::from_ymd_opt( 0, 0, 0 ).unwrap() ),
-			NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap()
-		);
+		assert_eq!( NormTime::from_timestamp_opt( time_norm_zero.timestamp() ).unwrap(), time_norm_zero );
+		assert_eq!( NormTime::from_ymd_opt( 0, 0, 0 ).unwrap(), time_norm_zero );
+		assert_eq!( NormTime::from_ymd_opt( 1, 0, 0 ).unwrap(), time_norm_zero + TimeDelta::seconds( 30_000_000 ) );
 	}
 
 	#[test]
@@ -255,11 +318,5 @@ mod tests {
 			NormTime::from( NaiveDate::from_ymd_opt( 2068, 1, 1 ).unwrap() ),
 			NormTime::from_ymd_opt( 0, 0, 0 ).unwrap()
 		);
-	}
-
-	#[test]
-	fn normtime_format() {
-		let date = NormTime::from_ymd_opt( 0, 1, 1 ).unwrap();
-		assert_eq!( date.to_string(), "0000-01-01N00:00:00");
 	}
 }
